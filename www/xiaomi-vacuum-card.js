d966c1d9fa2ff5869f918249acbcfdea443eded4
benct <ben@tomlin.no>
2019-10-17 16:05:56 +0200

    Upgrade xiaomi-vacuum-card module

--- www/xiaomi-vacuum-card.js
+++ www/xiaomi-vacuum-card.js
@@ -2,107 +2,108 @@ class XiaomiVacuumCard extends Polymer.Element {
 
     static get template() {
         return Polymer.html`
-<style>
-  .background {
-    background-repeat: no-repeat;
-    background-position: center center;
-    background-size: cover;
-  }
-  .title {
-    font-size: 20px;
-    padding: 16px 16px 0;
-    text-align: center;
-    white-space: nowrap;
-    text-overflow: ellipsis;
-    overflow: hidden;
-  }
-  .content {
-    cursor: pointer;
-  }
-  .flex {
-    display: flex;
-    align-items: center;
-    justify-content: space-evenly;
-  }
-  .button {
-    cursor: pointer;
-    padding: 16px;
-  }
-  .grid {
-    display: grid;
-    grid-template-columns: repeat(2, auto);
-  }
-  .grid-content {
-    display: grid;
-    align-content: space-between;
-    grid-row-gap: 6px;
-  }
-  .grid-left {
-    text-align: left;
-    font-size: 110%;
-    padding-left: 10px;
-    border-left: 2px solid var(--primary-color);
-  }
-  .grid-right {
-    text-align: right;
-    padding-right: 10px;
-    border-right: 2px solid var(--primary-color);
-  }
-</style>
-<ha-card hass="[[_hass]]" config="[[_config]]" class="background" style="[[backgroundImage]]">
-  <template is="dom-if" if="{{name}}">
-    <div class="title" style="[[contentText]]">[[name]]</div>
-  </template>
-  <div class="content grid" style="[[contentStyle]]" on-click="moreInfo">
-    <div class="grid-content grid-left">
-      <div>[[_config.labels.status]]: [[stateObj.attributes.status]]</div>
-      <div>[[_config.labels.battery]]: [[stateObj.attributes.battery_level]] %</div>
-      <div>[[_config.labels.mode]]: [[stateObj.attributes.fan_speed]]</div>
-    </div>
-    <template is="dom-if" if="{{showDetails}}">
-      <div class="grid-content grid-right" >
-        <div>[[_config.labels.main_brush]]: [[stateObj.attributes.main_brush_left]] [[_config.labels.hours]]</div>
-        <div>[[_config.labels.side_brush]]: [[stateObj.attributes.side_brush_left]] [[_config.labels.hours]]</div>
-        <div>[[_config.labels.filter]]: [[stateObj.attributes.filter_left]] [[_config.labels.hours]]</div>
-        <div>[[_config.labels.sensor]]: [[stateObj.attributes.sensor_dirty_left]] [[_config.labels.hours]]</div>
-      </div>
-    </template>
-  </div>
-  <template is="dom-if" if="{{showButtons}}">
-    <div class="flex" style="[[contentText]]">
-      <template is="dom-if" if="{{_config.buttons.start}}">
-        <div class="button" on-tap="startVaccum">
-          <ha-icon icon="mdi:play"></ha-icon>
-        </div>
-      </template>
-      <template is="dom-if" if="{{_config.buttons.pause}}">
-        <div class="button" on-tap="pauseVacuum">
-          <ha-icon icon="mdi:pause"></ha-icon>
-        </div>
-      </template>
-      <template is="dom-if" if="{{_config.buttons.stop}}">
-        <div class="button" on-tap="stopVacuum">
-          <ha-icon icon="mdi:stop"></ha-icon>
-        </div>
-      </template>
-      <template is="dom-if" if="{{_config.buttons.spot}}">
-        <div class="button" on-tap="cleanSpot">
-          <ha-icon icon="mdi:broom"></ha-icon>
-        </div>
-      </template>
-      <template is="dom-if" if="{{_config.buttons.locate}}">
-        <div class="button" on-tap="locateVacuum">
-          <ha-icon icon="mdi:map-marker"></ha-icon>
-        </div>
-      </template>
-      <template is="dom-if" if="{{_config.buttons.return}}">
-        <div class="button" on-tap="returnVacuum">
-          <ha-icon icon="mdi:home-map-marker"></ha-icon>
-        </div>
-      </template>
-    </div>
-  </template>
-</ha-card>`;
+          <style>
+            .background {
+              background-repeat: no-repeat;
+              background-position: center center;
+              background-size: cover;
+            }
+            .title {
+              font-size: 20px;
+              padding: 16px 16px 0;
+              text-align: center;
+              white-space: nowrap;
+              text-overflow: ellipsis;
+              overflow: hidden;
+            }
+            .content {
+              cursor: pointer;
+            }
+            .flex {
+              display: flex;
+              align-items: center;
+              justify-content: space-evenly;
+            }
+            .button {
+              cursor: pointer;
+              padding: 16px;
+            }
+            .grid {
+              display: grid;
+              grid-template-columns: repeat(2, auto);
+            }
+            .grid-content {
+              display: grid;
+              align-content: space-between;
+              grid-row-gap: 6px;
+            }
+            .grid-left {
+              text-align: left;
+              font-size: 110%;
+              padding-left: 10px;
+              border-left: 2px solid var(--primary-color);
+            }
+            .grid-right {
+              text-align: right;
+              padding-right: 10px;
+              border-right: 2px solid var(--primary-color);
+            }
+          </style>
+          <ha-card hass="[[_hass]]" config="[[_config]]" class="background" style="[[backgroundImage]]">
+            <template is="dom-if" if="{{name}}">
+              <div class="title" style="[[contentText]]">[[name]]</div>
+            </template>
+            <div class="content grid" style="[[contentStyle]]" on-click="moreInfo">
+              <div class="grid-content grid-left">
+                <div>[[getValue('status')]]</div>
+                <div>[[getValue('battery', ' %')]]</div>
+                <div>[[getValue('mode')]]</div>
+              </div>
+              <template is="dom-if" if="{{showDetails}}">
+                <div class="grid-content grid-right" >
+                  <div>[[computeValue('main_brush')]]</div>
+                  <div>[[computeValue('side_brush')]]</div>
+                  <div>[[computeValue('filter')]]</div>
+                  <div>[[computeValue('sensor')]]</div>
+                </div>
+              </template>
+            </div>
+            <template is="dom-if" if="{{showButtons}}">
+              <div class="flex" style="[[contentText]]">
+                <template is="dom-if" if="{{_config.buttons.start}}">
+                  <div class="button" on-tap="startVaccum">
+                    <ha-icon icon="mdi:play"></ha-icon>
+                  </div>
+                </template>
+                <template is="dom-if" if="{{_config.buttons.pause}}">
+                  <div class="button" on-tap="pauseVacuum">
+                    <ha-icon icon="mdi:pause"></ha-icon>
+                  </div>
+                </template>
+                <template is="dom-if" if="{{_config.buttons.stop}}">
+                  <div class="button" on-tap="stopVacuum">
+                    <ha-icon icon="mdi:stop"></ha-icon>
+                  </div>
+                </template>
+                <template is="dom-if" if="{{_config.buttons.spot}}">
+                  <div class="button" on-tap="cleanSpot">
+                    <ha-icon icon="mdi:broom"></ha-icon>
+                  </div>
+                </template>
+                <template is="dom-if" if="{{_config.buttons.locate}}">
+                  <div class="button" on-tap="locateVacuum">
+                    <ha-icon icon="mdi:map-marker"></ha-icon>
+                  </div>
+                </template>
+                <template is="dom-if" if="{{_config.buttons.return}}">
+                  <div class="button" on-tap="returnVacuum">
+                    <ha-icon icon="mdi:home-map-marker"></ha-icon>
+                  </div>
+                </template>
+              </div>
+            </template>
+          </ha-card>
+        `;
     }
 
     moreInfo() { this.fireEvent('hass-more-info'); }
@@ -164,10 +165,31 @@ class XiaomiVacuumCard extends Polymer.Element {
             return: true,
         };
 
+        const attributes = {
+            status: 'status',
+            battery: 'battery_level',
+            mode: 'fan_speed',
+            main_brush: 'main_brush_left',
+            side_brush: 'side_brush_left',
+            filter: 'filter_left',
+            sensor: 'sensor_dirty_left',
+        };
+
         const vendors = {
             xiaomi: {
                 image: '/local/img/vacuum.png',
                 details: true,
+            },
+            valetudo: {
+                image: '/local/img/vacuum.png',
+                details: true,
+                attributes: {
+                    status: 'state',
+                    main_brush: 'mainBrush',
+                    side_brush: 'sideBrush',
+                    filter: 'filter',
+                    sensor: 'sensor',
+                },
             },
             ecovacs: {
                 image: '/local/img/vacuum_ecovacs.png',
@@ -181,6 +203,21 @@ class XiaomiVacuumCard extends Polymer.Element {
                     pause: 'stop',
                     stop: 'turn_off',
                 },
+            },
+            ecovacs_ozmo: {
+                image: '/local/img/vacuum_ecovacs.png',
+                details: true,
+                service: {
+                    start: 'turn_on',
+                    pause: 'stop',
+                    stop: 'turn_off',
+                },
+                attributes: {
+                    main_brush: 'component_main_brush',
+                    side_brush: 'component_side_brush',
+                    filter: 'component_filter',
+                },
+                computeValue: v => (Number(v) / 100),
             }
         };
 
@@ -195,7 +232,21 @@ class XiaomiVacuumCard extends Polymer.Element {
 
         config.service = Object.assign({}, services, vendor.service);
         config.buttons = Object.assign({}, buttons, vendor.buttons, config.buttons);
+        config.attributes = Object.assign({}, attributes, vendor.attributes, config.attributes);
         config.labels = Object.assign({}, labels, config.labels);
+
+        this.getValue = (field, unit) => `${config.labels[field]}: ` + (config.attributes[field] in this.stateObj.attributes
+            ? this.stateObj.attributes[config.attributes[field]] + unit
+            : this._hass.localize('state.default.unavailable'));
+
+        this.computeValue = field => {
+            if (config.attributes[field] in this.stateObj.attributes) {
+                const value = this.stateObj.attributes[config.attributes[field]];
+                return `${config.labels[field]}: ${vendor.computeValue ? vendor.computeValue(value) : value} ${config.labels.hours}`;
+            } else {
+                return `${config.labels[field]}: - `;
+            }
+        };
 
         this.contentText = `color: ${config.image !== false ? 'white; text-shadow: 0 0 10px black;' : 'var(--primary-text-color)'}`;
         this.contentStyle = `padding: ${this.showButtons ? '16px 16px 4px' : '16px'}; ${this.contentText}`;
